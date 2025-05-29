9d21e5501faac795cbb90b3a4908c567a1aae43a
Hetsumani <62490862+Hetsumani@users.noreply.github.com>
2025-05-28 17:24:50 -0700

    Hacer click a las gemas

--- GemCrusher-0/JS/main.js
+++ GemCrusher-0/JS/main.js
@@ -6,10 +6,21 @@ const COLUMNAS = 8;
 
 let gemas = []
 
+let colores = ["red", "purple", "yellow", "blue", "green"];
+
 for(let i = 0; i < FILAS; i++) {
     gemas[i] = []
     for (let j = 0; j < COLUMNAS; j++){
-        gemas[i][j] = new Gema(i, j, "red");
+        const color = Math.floor(Math.random() * colores.length);        
+        gemas[i][j] = new Gema(i, j, colores[color]);
         tablero.appendChild(gemas[i][j].elDiv);
+
+        //Agregamos el eventListener
+        gemas[i][j].elDiv.addEventListener("click", clickGema);
     }
+}
+
+function clickGema(event){
+    const gemaClicada = event.target;
+    gemaClicada.classList.add("seleccionada");
 }
